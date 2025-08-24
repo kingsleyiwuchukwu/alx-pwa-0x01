f7c9b3ac6e8ac1e1a9e1d4271b092b82091c8a79
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-08-24 15:08:43 +0100

    linked manifest for PWA functionality

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+       </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
